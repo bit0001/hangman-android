cc9096f940730f958ac1b969093bebbe8e19fd42
m <lmiguelvargasf@gmail.com>
2016-11-22 16:49:42 -0500

    feat: Finish game when user guesses word
    When user gusses word directly, make game over.

--- app/src/main/java/com/mathsistor/m/hangman/Hangman.java
+++ app/src/main/java/com/mathsistor/m/hangman/Hangman.java
@@ -2,7 +2,13 @@ package com.mathsistor.m.hangman;
 
 public class Hangman {
 
-    private final String word = "word";
+    private final String word;
+    private boolean isGameOver;
+
+
+    public Hangman(String word) {
+        this.word = word;
+    }
 
     public void guessLetter(String guess) {
         if (guess.matches("[^A-Za-z]{1}")) {
@@ -16,7 +22,14 @@ public class Hangman {
         if (guess.length() > 1) {
             if (!guess.equals(word)) {
                 throw new RuntimeException("Incorrect word guess");
+            } else {
+                isGameOver = true;
+                return;
             }
         }
+    }
+
+    public boolean isGameOver() {
+        return isGameOver;
     }
 }
