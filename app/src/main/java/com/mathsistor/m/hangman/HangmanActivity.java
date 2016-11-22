ce89776891c1e4809d79a570ed479a4c64157b70
m <lmiguelvargasf@gmail.com>
2016-11-22 18:36:22 -0500

    feat: Clear edit text after user has guessed

--- app/src/main/java/com/mathsistor/m/hangman/HangmanActivity.java
+++ app/src/main/java/com/mathsistor/m/hangman/HangmanActivity.java
@@ -2,12 +2,25 @@ package com.mathsistor.m.hangman;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class HangmanActivity extends AppCompatActivity {
+
+    @BindView(R.id.text_field)
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hangman);
+        ButterKnife.bind(this);
+    }
+
+    public void guess(View view) {
+        editText.setText("");
     }
 }
