1a25f15362b61ab3f344c227bc4973359fa58e6b
m <lmiguelvargasf@gmail.com>
2016-11-22 16:16:47 -0500

    test: Test user enters a valid character

---
+++ app/src/test/java/com/mathsistor/m/hangman/HangmanTest.java
@@ -0,0 +1,31 @@
+package com.mathsistor.m.hangman;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class HangmanTest {
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+    private Hangman hangman;
+
+    @Before
+    public void setUp() {
+        hangman = new Hangman();
+    }
+
+    @Test
+    public void whenUserGuessesADigitAnExceptionIsThrown() {
+        exception.expect(RuntimeException.class);
+        hangman.guessLetter("9");
+    }
+
+    @Test
+    public void whenUserGuessesASpecialCharacterAnExceptionIsThrown() {
+        exception.expect(RuntimeException.class);
+        hangman.guessLetter("?");
+    }
+    
+}
