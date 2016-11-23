400529ea2ed0ce5a77dc219803eb86e5ad6e3fdd
m <lmiguelvargasf@gmail.com>
2016-11-22 23:14:25 -0500

    test: Test new game is created
    When user taps new game a new game is created.

---
+++ app/src/androidTest/java/com/mathsistor/m/hangman/NewGameTest.java
@@ -0,0 +1,49 @@
+package com.mathsistor.m.hangman;
+
+import android.support.annotation.NonNull;
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+public class NewGameTest {
+
+    @Rule
+    public ActivityTestRule<HangmanActivity> hangmanActivityTestRule =
+            new ActivityTestRule<>(HangmanActivity.class);
+
+    private HangmanActivity activity;
+
+    @Before
+    public void setUp() {
+        activity = hangmanActivityTestRule.getActivity();
+    }
+
+    @Test
+    public void givenUserIsPlayingWhenClickingNewGameANewGameIsStarted() {
+        String character = String.valueOf(activity.getGame().getWord().charAt(0));
+        onView(withId(R.id.text_field)).perform(typeText(character));
+        onView(withId(R.id.guess_button)).perform(click());
+        onView(withId(R.id.new_game_button)).perform(click());
+        onView(withId(R.id.word_to_guess)).check(matches(withText(getMaskedWord())));
+    }
+
+    @NonNull
+    private String getMaskedWord() {
+        StringBuilder maskedWord = new StringBuilder();
+        for (int i = 0; i < activity.getGame().getWord().length(); i++) {
+            maskedWord.append("?");
+        }
+
+        return maskedWord.toString();
+    }
+
+}
