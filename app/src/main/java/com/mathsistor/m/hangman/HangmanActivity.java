d467b1c793ef69838808b68046546eb0551054a0
m <lmiguelvargasf@gmail.com>
2016-11-22 21:11:50 -0500

    feat: Update guesses left when failing

--- app/src/main/java/com/mathsistor/m/hangman/HangmanActivity.java
+++ app/src/main/java/com/mathsistor/m/hangman/HangmanActivity.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.apache.commons.lang3.StringUtils;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -17,6 +19,12 @@ public class HangmanActivity extends AppCompatActivity {
     @BindView(R.id.word_to_guess)
     TextView wordToGuess;
 
+    @BindView(R.id.guessed_letters)
+    TextView guessedLetters;
+
+    @BindView(R.id.remaining_guesses)
+    TextView guessesLeft;
+
     private Hangman game;
 
     @Override
@@ -25,16 +33,26 @@ public class HangmanActivity extends AppCompatActivity {
         setContentView(R.layout.activity_hangman);
         ButterKnife.bind(this);
         game = new Hangman("word");
+        updateUI();
+    }
+
+    private void updateUI() {
         wordToGuess.setText(game.getMaskedWord());
+        guessesLeft.setText("(" + game.getGuessesLeft() +  getString(R.string.guesses_left));
     }
 
     public void guess(View view) {
         game.guess(editText.getText().toString());
         wordToGuess.setText(game.getMaskedWord());
+        guessedLetters.setText(
+                getResources().getString(R.string.you_have_guessed) + " " +
+                        StringUtils.join(game.getGuessedLetters(), ",") + ".");
+        guessesLeft.setText("(" + game.getGuessesLeft() +  getString(R.string.guesses_left));
         editText.setText("");
     }
 
     public Hangman getGame() {
         return game;
     }
+
 }
